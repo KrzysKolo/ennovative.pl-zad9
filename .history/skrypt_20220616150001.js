182f9f2769b170c31439f925cb04c46ee597d6dd
KrzysKolo <krzysztofa.kolodziejczak@gmail.com>
2022-06-16 15:00:28 +0200

    zadanie 10

--- .history/skrypt_20220616150001.js
+++ .history/skrypt_20220616150001.js
@@ -5,16 +5,17 @@ const newTabColorLength = [] //istotna tablica do zadania 10
 const TabWithObject = [] //tablica do zadania 10, która prezentuje obiekt składający się z danych o nazwie koloru i jego długości
 
 newTabColor.push(tabColor.filter(item => item.length > 4));
-console.log(newTabColor);
+console.log(newTabColor)
+
 
 newTabColor[0].forEach(item => {
   let dane = {
     name: item,
     wordLength: item.length
   }
-  TabWithObject.push(dane);
+  TabWithObject.push(dane)
   newTabColorLength.push(item.length);
-});
+})
 
-console.log(TabWithObject);
-console.log(newTabColorLength);
+console.log(TabWithObject)
+console.log(newTabColorLength)
